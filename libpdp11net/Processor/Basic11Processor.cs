7f7a8168f0f8c14577f348ca1d7d56e05a7d071a
Martin Sundhaug <martin@sundhaug@gmail.com>
2012-04-15 14:54:55 +0200

    Added basic PDP-11 processor

---
+++ libpdp11net/Processor/Basic11Processor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace libpdp11net.Processor
+{
+    internal class Basic11Processor
+    {
+        ushort[] Register = new ushort[8];
+        Thread RunningThread;
+
+        public Basic11Processor()
+        {
+            RunningThread = new Thread(Run);
+        }
+
+        public void Step()
+        {
+        }
+
+        public void Run()
+        {
+            while (true) Step();
+        }
+
+        public void Start()
+        {
+            if (!RunningThread.IsAlive) RunningThread.Start();
+        }
+
+        public void Stop()
+        {
+            if (RunningThread.IsAlive) RunningThread.Abort();
+        }
+    }
+}
